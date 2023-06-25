9fe1287623f72f1ee9387f39ec9eab8e1ddfb975
Les <les.floresbae@gmail.com>
2023-06-25 11:09:54 -0600

    Modificaciónes al ejercicio

--- formulario.js
+++ formulario.js
@@ -1,8 +1,8 @@
-var formulario = document.querySelector("#form")
+var formulario = document.querySelector(".formulario")/*SE MODIFICA EL NOMBRE FORM*/
 
 formulario.onsubmit = function(e) {
 
-  e.prevent();
+  e.preventDefault();/*SE AGREGA PREVENDEFAULT */
   
   var n = formulario.elements[0]
   var e = formulario.elements[1]
@@ -17,7 +17,7 @@ formulario.onsubmit = function(e) {
   console.log(nacionalidad)
 
   if (nombre.length === 0) {
-    n.classList.add("error")
+    n.classList.add("error")/*SE MODIFICA ADDED*/
   }
   if (edad < 18 || edad > 120) {
     e.classList.add("error")
@@ -55,24 +55,26 @@ function agregarInvitado(nombre, edad, nacionalidad) {
 var lista = document.getElementById("lista-de-invitados")
 
 var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
+elementoLista.classList.add("elemento-lista")
 lista.appendChild(elementoLista)
 
-var spanNombre = document.createElement("span")
+/*SE COMENTA ESTE PEDAZO PARA QUITAR EL NOMBRE REPETIDO*/
+/*var spanNombre = document.createElement("span")
 var inputNombre = document.createElement("input")
 var espacio = document.createElement("br")
+
 spanNombre.textContent = "Nombre: "
 inputNombre.value = nombre 
 elementoLista.appendChild(spanNombre)
 elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+elementoLista.appendChild(espacio)*/
 
 function crearElemento(descripcion, valor) {
 var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
+var inputNombre = document.createElement("span")
 var espacio = document.createElement("br")
 spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
+inputNombre.textContent = valor 
 elementoLista.appendChild(spanNombre)
 elementoLista.appendChild(inputNombre)
 elementoLista.appendChild(espacio)
@@ -83,9 +85,9 @@ crearElemento("Edad", edad)
 crearElemento("Nacionalidad", nacionalidad)
 
 
-var botonBorrar = document.createElement("button")
+/*var botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
+botonBorrar.id = "boton-borrar"*/
 var corteLinea = document.createElement("br")
 elementoLista.appendChild(corteLinea)
 elementoLista.appendChild(botonBorrar);
